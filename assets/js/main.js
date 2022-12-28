fd8b3d66209d48e11c8c960a78efd0f5d3cf1bcc
vinayakmurgod <110614434+vinayakmurgod@users.noreply.github.com>
2022-12-28 22:57:31 +0530

    Fix owl carousel and add toggle menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 console.log("connected");
 
 $(document).ready(() => {
+
+
+  //Menu toggle 
+  $("#menu li").click(()=>{
+    console.log("menu button clicked");
+    $("#menu").toggleClass("toggle");
+  })
+
   // appointment form
   $(".book-apt-btn").click(() => {
     console.log("button clicked");
@@ -12,24 +20,57 @@ $(document).ready(() => {
   });
 
   // Owl Carousel
-  $(".owl-carousel").owlCarousel({
-    loop: false,
-    margin: 10,
-    nav: false,
-    responsive: {
-      0: {
-        items: 1,
-        // loop: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
-      },
-      600: {
-        items: 3,
-      },
-      1000: {
-        items: 3,
-      },
-    },
+  // $(".owl-carousel").owlCarousel({
+  //   loop: false,
+  //   margin: 10,
+  //   nav: false,
+  //   responsive: {
+  //     0: {
+  //       items: 1,
+  //       // loop: true,
+  //       autoplay: true,
+  //       autoplayTimeout: 3000,
+  //       autoplayHoverPause: true,
+  //     },
+  //     600: {
+  //       items: 3,
+  //     },
+  //     1000: {
+  //       items: 3,
+  //     },
+  //   },
+  // });
+
+  $(function () {
+    var owl = $(".owl-carousel"),
+      owlOptions = {
+        loop: false,
+        margin: 10,
+        responsive: {
+          0: {
+            items: 2,
+          },
+        },
+      };
+
+    if ($(window).width() < 854) {
+      var owlActive = owl.owlCarousel(owlOptions);
+    } else {
+      owl.addClass("off");
+    }
+
+    $(window).resize(function () {
+      if ($(window).width() < 854) {
+        if ($(".owl-carousel").hasClass("off")) {
+          var owlActive = owl.owlCarousel(owlOptions);
+          owl.removeClass("off");
+        }
+      } else {
+        if (!$(".owl-carousel").hasClass("off")) {
+          owl.addClass("off").trigger("destroy.owl.carousel");
+          owl.find(".owl-stage-outer").children(":eq(0)").unwrap();
+        }
+      }
+    });
   });
 });
